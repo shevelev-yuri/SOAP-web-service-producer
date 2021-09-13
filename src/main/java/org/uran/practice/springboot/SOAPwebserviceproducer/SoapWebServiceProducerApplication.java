31f3066febe6f67546959f14a0664c42e84f5578
ishevele <iurii.shevelev@t-system.com>
2021-09-13 13:41:37 +0300

    Initial commit

---
+++ src/main/java/org/uran/practice/springboot/SOAPwebserviceproducer/SoapWebServiceProducerApplication.java
@@ -0,0 +1,13 @@
+package org.uran.practice.springboot.SOAPwebserviceproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebServiceProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebServiceProducerApplication.class, args);
+	}
+
+}
